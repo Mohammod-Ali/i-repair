508528e3c549000c9053f54540cd1272a9560ea8
Mohammod Ali <mohammodali651@gmail.com>
2024-02-08 23:20:27 +0600

    navbar ready and google font added

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}) => {
     return (
-        <div className="w-full  max-w-[1220px] px-[20px] mx-auto bg-green-500">{
+        <div className="w-full  max-w-[1220px] px-[20px] mx-auto ">{
             children
         }
     </div>
