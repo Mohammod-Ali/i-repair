e8f00a94ee55019fb7e9ff317587c39fc606aebf
Mohammod Ali <mohammodali651@gmail.com>
2024-02-14 23:58:38 +0600

    services section added

---
+++ src/pages/home/ServicesSection.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/Container";
+
+const ServicesSection = () => {
+    return (
+        <Container className="my-40">
+            <h1>This is Services Section</h1>
+        </Container>
+    );
+};
+
+export default ServicesSection;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import AboutUsSection from "./AboutUsSection";
 import HeroSection from "./HeroSection";
+import ServicesSection from "./ServicesSection";
 
 const index = () => {
     return (
         <div>
             <HeroSection></HeroSection>
             <AboutUsSection></AboutUsSection>
+            <ServicesSection></ServicesSection>
         </div>
     );
 };
