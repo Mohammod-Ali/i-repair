712786908de564332c2bf28f64860b79531259f9
Mohammod Ali <mohammodali651@gmail.com>
2024-02-15 00:14:43 +0600

    service section layout added

--- src/pages/home/ServicesSection.tsx
+++ src/pages/home/ServicesSection.tsx
@@ -1,11 +1,27 @@
 import Container from "@/components/Container";
 
 const ServicesSection = () => {
-    return (
-        <Container className="my-40">
-            <h1>This is Services Section</h1>
-        </Container>
-    );
+  return (
+    <Container className="my-40">
+      <div className="text-center">
+        <h1>Services that we provide.</h1>
+        <p className="max-w-[80ch] mt-10 mb-20">
+          All out technicians are fully qualified and licensed. Moreover, they
+          are incredibly skillful and proficient in various aspects of computer
+          repair.
+        </p>
+      </div>
+      <div className="grid grid-cols-12 gap-[20px]">
+        <div className="bg-red-500 h-[415px] rounded-2xl col-span-12"></div>
+        <div className="bg-red-500 h-[415px] rounded-2xl col-span-6 lg:col-span-5"></div>
+        <div className="bg-red-500 h-[415px] rounded-2xl col-span-6 lg:col-span-7"></div>
+        <div className="bg-red-500 h-[415px] rounded-2xl col-span-12"></div>
+        <div className="bg-red-500 h-[415px] rounded-2xl col-span-12 md:col-span-6 lg:col-span-4"></div>
+        <div className="bg-red-500 h-[415px] rounded-2xl col-span-12  md:col-span-6 lg:col-span-4"></div>
+        <div className="bg-red-500 h-[415px] rounded-2xl col-span-12  lg:col-span-4"></div>
+      </div>
+    </Container>
+  );
 };
 
 export default ServicesSection;
