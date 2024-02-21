fad953114fd65ac9073905ea61d64bb74f56272e
Mohammod Ali <mohammodali651@gmail.com>
2024-02-22 01:05:13 +0600

    admin layout responsive added

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,13 +1,55 @@
+import { cn } from "@/lib/utils";
+import { LayoutDashboard } from "lucide-react";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <aside className="bg-light-gray col-span-2 h-screen sticky top-0 left-0 overflow-auto p-5">
-        <nav className="flex flex-col">
-            <NavLink to='/admin' className='p-3 bg-gray rounded-md hover:bg-dark-gray hover:text-white transition-all truncate '>Dashboard</NavLink>
-            <NavLink to='/admin/add-service'>Add Service</NavLink>
-            <NavLink to='/admin/service-list'>Service List</NavLink>
-        </nav>
+    <aside className="bg-light-gray col-span-2 h-screen sticky top-0 left-0 overflow-auto p-4 lg:p-5">
+      <nav className="flex flex-col gap-2">
+        <NavLink
+          to="/admin/dashboard"
+          className={({ isActive }) =>
+            cn(
+              "p-3 bg-gray rounded-md hover:bg-dark-gray hover:text-white transition-all  flex items-center gap-2",
+              {
+                "hover:bg-dark-gray hover:text-white": isActive,
+              }
+            )
+          }
+        >
+          <LayoutDashboard className="shrink-0"></LayoutDashboard>
+          <span className="truncate">Dashboard</span>
+        </NavLink>
+        <NavLink
+          to="/admin/add-service"
+          className={({ isActive }) =>
+            cn(
+              "p-3 bg-gray rounded-md hover:bg-dark-gray hover:text-white transition-all  flex items-center gap-2",
+              {
+                "hover:bg-dark-gray hover:text-white": isActive,
+              }
+            )
+          }
+        >
+          <LayoutDashboard className="shrink-0"></LayoutDashboard>
+          <span className="truncate">Add Service</span>
+        </NavLink>
+        <NavLink
+          to="/admin/service-list"
+          className={({ isActive }) =>
+            cn(
+              "p-3 bg-gray rounded-md hover:bg-dark-gray hover:text-white transition-all  flex items-center gap-2",
+              {
+                "hover:bg-dark-gray hover:text-white": isActive,
+              }
+            )
+          }
+        >
+          <LayoutDashboard className="shrink-0"></LayoutDashboard>
+          <span className="truncate">Service List</span>
+        </NavLink>
+        {/* <NavLink to=''></NavLink> */}
+      </nav>
     </aside>
   );
 };
