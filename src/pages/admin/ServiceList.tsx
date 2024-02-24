df04d4c9abfb3ee907d360f1f9120c6ae993e672
Mohammod Ali <mohammodali651@gmail.com>
2024-02-24 23:00:32 +0600

    api added in servicelist component

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import { json } from "stream/consumers";
+
 const ServiceList = () => {
+
+const [data, setData] = useState([])
+
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+  .then( data => setData(data))
+},[])
+
   return <div>
         This is service components
   </div>;
