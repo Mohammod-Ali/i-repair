19e6b5dd4fb204747ae70568ce48afc606b7008b
Mohammod Ali <mohammodali651@gmail.com>
2024-02-29 23:28:20 +0600

    shadcn table added

--- src/components/ServiceTyles/DataRecovery.tsx
+++ src/components/ServiceTyles/DataRecovery.tsx
@@ -1,16 +1,15 @@
 import useScrollGrow from "@/hooks/ScrollGrowHook";
 import { motion } from "framer-motion";
 
-
-
 const DataRecovery = () => {
-    const { style, componentRef } = useScrollGrow();
-    return (
-        <motion.div 
-        style={style}
+  const { style, componentRef } = useScrollGrow();
+  return (
+    <motion.div
+      style={style}
       ref={componentRef}
-        className="bg-red-500 h-[415px] rounded-2xl col-span-6 lg:col-span-7"></motion.div>
-    );
+      className="bg-red-500 h-[415px] rounded-2xl col-span-6 lg:col-span-7"
+    ></motion.div>
+  );
 };
 
 export default DataRecovery;
